fb6b4ae5e2764d36a675c423e97fc7b45f013012
kimkiwon <geniikw@gmail.com>
2018-02-16 19:56:13 +0900

    Start

---
+++ Assets/CoroutineChain/Editor/CoroutineTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class CoroutineTest {
+
+	[Test]
+	public void CoroutineTestSimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator WaitTest() {
+
+        var startTime = Time.realtimeSinceStartup;
+        var endTime = 0f;
+        CoroutineChain.Start
+            .Wait(0.1f)
+            .Call(() => endTime = Time.realtimeSinceStartup);
+
+        Assert.GreaterOrEqual(endTime, 0.1f);
+
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
