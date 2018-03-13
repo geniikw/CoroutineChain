c9fa9f72d758bfca2c88d188a998f80c23d58abf
kimkiwon <geniikw@gmail.com>
2018-03-13 17:52:08 +0900

    Revert "Revert "apply memoryPool""
    This reverts commit 4105f94c4a250b50d08d530226c58bdb1425e38c.

---
+++ Assets/CoroutineChain/Chain.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace geniikw.CChain
+{
+    public class Chain 
+    {
+        EType type;
+        MonoBehaviour player;
+        IEnumerator routine;
+        IEnumerator[] parallelRoutine;
+        System.Action action;
+        
+        public Coroutine Play()
+        {
+            switch (type)
+            {
+                default:
+                case EType.NonCoroutine:
+                    action();
+                    return null;
+                case EType.Parallel:
+                    return player.StartCoroutine(Parallel(parallelRoutine));
+                case EType.Single:
+                    return player.StartCoroutine(routine);
+            }
+        }
+
+        public Chain SetupRoutine(IEnumerator routine, MonoBehaviour player)
+        {
+            type = EType.Single;
+            this.player = player;
+            this.routine = routine;
+            return this;
+        }
+
+        public Chain SetupParallel(IEnumerator[] routines, MonoBehaviour player)
+        {
+            type = EType.Parallel;
+            this.player = player;
+            this.parallelRoutine = routines;
+            return this;
+        }
+
+        public Chain SetupNon(System.Action action, MonoBehaviour player)
+        {
+            type = EType.NonCoroutine;
+            this.player = player;
+            this.action = action;
+            return this;
+        }
+
+        public void Clear()
+        {
+            player = null;
+            routine = null;
+            action = null;
+            parallelRoutine = null;
+        }
+
+        IEnumerator Parallel(IEnumerator[] routines)
+        {
+            var all = 0;
+            foreach (var r in routines)
+                all++;
+
+            var c = 0;
+            foreach (var r in routines)
+                player.StartChain()
+                    .Play(r)
+                    .Call(() => c++);
+
+            while (c < all)
+                yield return null;
+        }
+        
+        public enum EType
+        {
+            Single,
+            Parallel,
+            NonCoroutine
+        }
+    }
+}
--- Assets/CoroutineChain/ChainBase.cs
+++ Assets/CoroutineChain/ChainBase.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ELogType
+{
+    NORMAL,
+    WARRNING,
+    ERROR
+}
+
 namespace geniikw.CChain
 {
     public interface IChain
@@ -11,9 +18,13 @@ namespace geniikw.CChain
     
     public class ChainBase : CustomYieldInstruction
     {
-        MonoBehaviour _mono;
+        public static MemoryPool<ChainBase> BasePool = new MemoryPool<ChainBase>(null,c => c.Clear());
+        public static MemoryPool<Chain> ChainPool = new MemoryPool<Chain>(null,c => c.Clear());
+        
+        MonoBehaviour _player;
 
-        Queue<IChain> m_chainQueue= new Queue<IChain>();
+        Queue<Chain> m_chainQueue = new Queue<Chain>();
+        
         bool m_isPlay = true;
 
         public override bool keepWaiting
@@ -24,69 +35,88 @@ namespace geniikw.CChain
             }
         }
 
-        public ChainBase(MonoBehaviour mono)
+        public ChainBase Setup(MonoBehaviour player)
         {
-            _mono = mono;
-            _mono.StartCoroutine(Routine());
+            m_isPlay = true;
+            _player = player;
+            _player.StartCoroutine(Routine());
+            return this;
+        }
+
+        void Clear()
+        {
+            _player = null;
+            m_chainQueue.Clear();
         }
         
         IEnumerator Routine()
         {
-            //Comment
-            //처음 생성자에서 StartCoroutine을 할땐 큐가 비어있다. 그다음 체인에서
-            //큐를 쌓는데 그동안 기다려야 한다.
-            //만약 사용자가 빈 Chain을 실행하는 경우 끝나지 않는 Coroutine이 됨으로 
-            //1초간만 기다린다.
-            
-            // var startTime = Time.realtimeSinceStartup;
-            // while (m_chainQueue.Count == 0 && Time.realtimeSinceStartup - startTime < 1)
-                yield return null;
+            yield return null;
                         
             while (m_chainQueue.Count > 0)
             {
-                var cr = m_chainQueue.Dequeue().Play(_mono);
+                var chain = m_chainQueue.Dequeue();
+                var cr = chain.Play();
                 if(cr != null)
                     yield return cr;
+                ChainPool.Despawn(chain);
             }
+
             m_isPlay = false;
+            BasePool.Despawn(this);
         }
 
         public ChainBase Play(IEnumerator routine)
         {
-            m_chainQueue.Enqueue(new Play(routine));
+            m_chainQueue.Enqueue(ChainPool.Spawn().SetupRoutine(routine, _player));
             return this;
         }
 
         public ChainBase Wait(float waitSec)
         {
-            m_chainQueue.Enqueue(new Wait(waitSec));
+            m_chainQueue.Enqueue(ChainPool.Spawn().SetupRoutine(WaitRoutine(waitSec), _player));
             return this;
         }
 
         public ChainBase Parallel(params IEnumerator[] routines)
         {
-            m_chainQueue.Enqueue(new Parallel(routines));
+            m_chainQueue.Enqueue(ChainPool.Spawn().SetupParallel(routines, _player));
             return this;
         }
 
         public ChainBase Sequential(params IEnumerator[] routines)
         {
             foreach (var routine in routines)
-                m_chainQueue.Enqueue(new Play(routine));
+                m_chainQueue.Enqueue(ChainPool.Spawn().SetupRoutine(routine, _player));
             return this;
         }
 
         public ChainBase Log(string log, ELogType type = ELogType.NORMAL)
         {
-            m_chainQueue.Enqueue(new Log(log, type));
+            System.Action action;
+            switch (type)
+            {
+                default:
+                case ELogType.NORMAL:
+                    action = ()=> Debug.Log(log); break;
+                case ELogType.WARRNING:
+                    action = ()=>Debug.LogWarning(log); break;
+                case ELogType.ERROR:
+                    action = ()=> Debug.LogError(log); break;
+            }
+            m_chainQueue.Enqueue(ChainPool.Spawn().SetupNon(action, _player));
             return this;
         }
 
-        public ChainBase Call(System.Action f)
+        public ChainBase Call(System.Action action)
         {
-            m_chainQueue.Enqueue(new Call(f));
+            m_chainQueue.Enqueue(ChainPool.Spawn().SetupNon(action, _player));
             return this;
         }
 
+        IEnumerator WaitRoutine(float wait)
+        {
+            yield return new WaitForSeconds(wait);
+        }
     }
 }
